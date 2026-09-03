d7d3ad58649441e3ee53bfe0e4f37e33615e3257
vasilev38998 <vasilev38998@bk.ru>
2026-09-03 16:42:00 +0800

    feat: migrate existing Evotor sales to Irkutsk before new imports

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -20,6 +20,11 @@ date_default_timezone_set('Asia/Irkutsk');
 require_once __DIR__ . '/db.php';
 require_once __DIR__ . '/settings.php';
 ensure_settings_tables();
+
+// Старые чеки ранее сохранялись в МСК. Одноразово переводим их в Иркутск (+5 часов)
+// до того, как новая версия сможет импортировать очередной чек уже в правильном часовом поясе.
+migrate_evotor_times_to_irkutsk_once();
+
 $config['app']['timezone'] = app_timezone();
 $config['app']['currency'] = app_currency();
 date_default_timezone_set($config['app']['timezone']);
