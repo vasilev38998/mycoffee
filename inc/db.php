52f7270d95875e155fd17a5519dc5131bdcd3dc4
vasilev38998 <vasilev38998@bk.ru>
2026-09-03 14:49:54 +0800

    feat: add PDO database connection

---
+++ inc/db.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+function db(): PDO
+{
+    static $pdo = null;
+    global $config;
+
+    if ($pdo instanceof PDO) {
+        return $pdo;
+    }
+
+    $db = $config['db'];
+    $dsn = sprintf(
+        'mysql:host=%s;dbname=%s;charset=%s',
+        $db['host'],
+        $db['name'],
+        $db['charset'] ?? 'utf8mb4'
+    );
+
+    $pdo = new PDO($dsn, $db['user'], $db['pass'], [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]);
+
+    return $pdo;
+}
