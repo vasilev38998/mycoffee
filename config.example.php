8bc168078d89e11cb40768ca159eed77f5af4a49
vasilev38998 <vasilev38998@bk.ru>
2026-09-03 14:49:24 +0800

    chore: add hosting configuration example

---
+++ config.example.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'app' => [
+        'name' => 'MyCoffee',
+        'timezone' => 'Asia/Irkutsk',
+        'currency' => '₽',
+    ],
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'YOUR_DB_NAME',
+        'user' => 'YOUR_DB_USER',
+        'pass' => 'YOUR_DB_PASSWORD',
+        'charset' => 'utf8mb4',
+    ],
+];
