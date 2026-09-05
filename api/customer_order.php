4262687a80cc37eb518ca25fd780149e58ded501
vasilev38998 <vasilev38998@bk.ru>
2026-09-05 18:28:00 +0800

    feat: support scheduled pickup in customer orders

--- api/customer_order.php
+++ api/customer_order.php
@@ -10,8 +10,23 @@ customer_api_guard_origin();
 if(strtoupper((string)($_SERVER['REQUEST_METHOD']??'GET'))!=='POST')customer_api_reply(405,['ok'=>false,'error'=>'Method not allowed']);
 try{
     $data=customer_api_json();
+    $delay=0;
+    $comment=(string)($data['comment']??'');
+    if(preg_match('/\[\[pickup:(\d{1,3})\]\]/',$comment,$m)){
+        $delay=(int)$m[1];
+        $data['comment']=trim(preg_replace('/\s*\[\[pickup:\d{1,3}\]\]\s*/',' ',$comment)??$comment);
+    }
+    if($delay<0||$delay>180)throw new RuntimeException('Некорректное время получения заказа.');
+    $allowed=[0,15,30,45,60,90,120];
+    if(!in_array($delay,$allowed,true))throw new RuntimeException('Выберите доступное время получения.');
     $customer=customer_auth_current();
     $order=customer_order_create($data,$customer);
+    if($delay>0&&!empty($order['order_id'])){
+        $promised=date('Y-m-d H:i:s',time()+$delay*60);
+        db()->prepare('UPDATE online_orders SET promised_at=? WHERE id=?')->execute([$promised,(int)$order['order_id']]);
+        $order['promised_at']=$promised;
+        $order['pickup_delay_minutes']=$delay;
+    }
     customer_api_reply(201,['ok'=>true,'order'=>$order]);
 }catch(JsonException $e){
     customer_api_reply(400,['ok'=>false,'error'=>'Некорректный JSON.']);
