c7b567731e3c9f1b28fdcd92910d52ef781bb94a
vasilev38998 <vasilev38998@bk.ru>
2026-09-25 14:41:26 +0800

    chore(customer): refresh PWA cache for modifier redesign

--- customer/sw.js
+++ customer/sw.js
@@ -1,6 +1,6 @@
-const CACHE='kapouch-pwa-v47';
-const PREVIOUS_CACHE='kapouch-pwa-v46';
-const PREVIOUS_PREVIOUS_CACHE='kapouch-pwa-v45';
+const CACHE='kapouch-pwa-v48';
+const PREVIOUS_CACHE='kapouch-pwa-v47';
+const PREVIOUS_PREVIOUS_CACHE='kapouch-pwa-v46';
 const SIXTH_DRINK_CONTRACT_CACHE='kapouch-pwa-v44';
 const LOCAL_NAMES_CONTRACT_CACHE='kapouch-pwa-v43';
 const LOCAL_NAMES_CACHE='kapouch-pwa-v41';
